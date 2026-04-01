0c65bf705a8e97e256956e07ee841a09c80f3aed
Violeta Villavicencio <violetapvillavicencio@gmail.com>
2026-04-01 17:23:38 -0300

    Add comments about interface modification complexity
    Added comments regarding the complexity of modifying the interface and implementing classes.

--- PracticaDeRepaso/ejercicio2/Jugada.java
+++ PracticaDeRepaso/ejercicio2/Jugada.java
@@ -13,5 +13,7 @@ public interface Jugada {
 	public String jugarContraLagarto();
 	
 	public String jugarContraSpock();
-	
+
+	// en el segundo punto la complejidad es la de agregar un elemento en la jugada. Hay que modificar la interfaz y cada una de las clases que 
+	// la implementan, esto lo vamos a poder resolver con un patron más adelante..
 }
