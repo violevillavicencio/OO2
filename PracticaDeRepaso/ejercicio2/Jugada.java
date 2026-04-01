2a575b0a71f0e2788e2920ee9c90244c1916cd5e
Violeta Villavicencio <violetapvillavicencio@gmail.com>
2026-04-01 17:18:45 -0300

    Create Jugada.java

---
+++ PracticaDeRepaso/ejercicio2/Jugada.java
@@ -0,0 +1,17 @@
+package io.github.unlp_oo.archetype;
+
+public interface Jugada {
+	
+	public String jugarContra(Jugada otrajugada);
+	
+	public String jugarContraPiedra();
+	
+	public String jugarContraPapel();
+	
+	public String jugarContraTijera();
+	
+	public String jugarContraLagarto();
+	
+	public String jugarContraSpock();
+	
+}
