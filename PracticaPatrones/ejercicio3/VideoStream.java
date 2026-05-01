09070259829ff2dc1fda226c84ed75441c551ab0
Violeta Villavicencio <violetapvillavicencio@gmail.com>
2026-05-01 20:53:18 -0300

    Create VideoStream.java

---
+++ PracticaPatrones/ejercicio3/VideoStream.java
@@ -0,0 +1,9 @@
+package io.github.unlp_oo.archetype;
+
+//libreria externa con metodo reproduce 
+public class VideoStream {
+	
+	public void reproduce () {
+		System.out.println("Reproducir video stream");
+	}
+}
