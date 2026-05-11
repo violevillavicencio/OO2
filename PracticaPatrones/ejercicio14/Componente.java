24440703a8d9d582142a9801fee51acb04f89cb5
Violeta Villavicencio <violetapvillavicencio@gmail.com>
2026-05-11 17:37:55 -0300

    Add Componente class with properties and methods

---
+++ PracticaPatrones/ejercicio14/Componente.java
@@ -0,0 +1,32 @@
+package io.github.unlp_oo.archetype;
+
+public class Componente {
+	private String nombre; 
+	private String descripcion; 
+	private double precio; 
+	private double consumo;
+	
+	public Componente(String nombre, String descripcion, double precio, double consumo) {
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.consumo = consumo;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public double getConsumo() {
+		return consumo;
+	}
+
+}
