ee987b02418079b3708d79b981f75c2c6860d1df
Violeta Villavicencio <violetapvillavicencio@gmail.com>
2026-05-11 19:33:29 -0300

    Update BasicoBuilder.java

--- PracticaPatrones/ejercicio14/BasicoBuilder.java
+++ PracticaPatrones/ejercicio14/BasicoBuilder.java
@@ -8,35 +8,35 @@ public class BasicoBuilder extends EquipoBuilder {
 
     @Override
     public void setProcesador() {
-        presupuesto.setProcesador(
+        presupuesto.addComponente(
             catalogo.getComponente("Procesador Basico")
         );
     }
 
     @Override
     public void setMemoriaRAM() {
-        presupuesto.setMemoriaRAM(
+        presupuesto.addComponente(
             catalogo.getComponente("8 GB")
         );
     }
 
     @Override
     public void setDisco() {
-        presupuesto.setDisco(
+        presupuesto.addComponente(
             catalogo.getComponente("HDD 500 GB")
         );
     }
 
     @Override
     public void setTarjetaGrafica() {
-        presupuesto.setTarjetaGrafica(
+        presupuesto.addComponente(
             catalogo.getComponente("Integrada")
         );
     }
 
     @Override
     public void setGabinete() {
-        presupuesto.setGabinete(
+        presupuesto.addComponente(
             catalogo.getComponente("Gabinete Estandar")
         );
     }
