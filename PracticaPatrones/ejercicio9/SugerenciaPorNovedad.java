9f97d22ed9caeb55e2e36efc2a7a5d715f5f063f
Violeta Villavicencio <violetapvillavicencio@gmail.com>
2026-05-12 00:27:55 -0300

    Create SugerenciaPorNovedad.java

---
+++ PracticaPatrones/ejercicio9/SugerenciaPorNovedad.java
@@ -0,0 +1,17 @@
+package io.github.unlp_oo.archetype;
+import java.util.Comparator;
+import java.util.List;
+
+public class SugerenciaPorNovedad implements EstrategiaSugerencia {
+
+    @Override
+    public List<Pelicula> sugerir(Decodificador deco) {
+        return deco.getGrilla().stream()
+                .filter(p ->!deco.getReproducidas().contains(p))
+                .sorted(Comparator.comparing(Pelicula::getEstreno)
+                .reversed())
+                .limit(3)
+                .toList();
+    }
+    
+}
