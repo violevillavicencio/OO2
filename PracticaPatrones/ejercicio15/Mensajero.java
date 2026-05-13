aefff7e6abb5b0dd1004238b0ab4b41b61e63089
Violeta Villavicencio <violetapvillavicencio@gmail.com>
2026-05-12 23:38:21 -0300

    Update Mensajero.java

--- PracticaPatrones/ejercicio15/Mensajero.java
+++ PracticaPatrones/ejercicio15/Mensajero.java
@@ -1,1 +1,21 @@
+package io.github.unlp_oo.archetype;
 
+public class Mensajero {
+	private Cifrador cifrador;
+
+	public Mensajero(Cifrador cifrador) {
+		this.cifrador = cifrador;
+	}
+
+	public void setCifrador(Cifrador cifrador) {
+		this.cifrador = cifrador;
+	}
+
+	public String enviar(String mensaje) {
+        return cifrador.cifrar(mensaje);
+	}
+
+	public String recibir(String mensaje) {
+		return cifrador.descifrar(mensaje);
+	}
+}
