1e1e27077033d4bee13bad1159b0428e3771103b
Violeta Villavicencio <violetapvillavicencio@gmail.com>
2026-05-16 02:10:00 -0300

    Create HomeWeatherStation.java

---
+++ PracticaPatrones/ejercicio21/HomeWeatherStation.java
@@ -0,0 +1,31 @@
+package io.github.unlp_oo.archetype;
+
+import java.util.List;
+
+// esta seria una clase q disponemos ya implementada
+public class HomeWeatherStation {
+	
+	//retorna la temperatura en grados Fahrenheit. 
+	public double getTemperatura() {
+		return 0;}
+
+	//retorna la presión atmosférica en hPa
+	public double getPresion(){
+		return 0;}
+
+	//retorna la radiación solar
+	public double getRadiacionSolar(){
+		return 0;}
+
+	//retorna una lista con todas las temperaturas sensadas hasta el momento, en grados Fahrenheit
+	public List<Double> getTemperaturas(){
+		return null;}
+
+	//retorna  un reporte de todos los datos: temperatura, presión, y radiación solar.
+	public String displayData(){
+	return "Temperatura F: " + this.getTemperatura() +
+	   	  "Presión atmosf: " + this.getPresion() +
+	   	  "Radiación solar: " + this.getRadiacionSolar();
+	}
+
+}
